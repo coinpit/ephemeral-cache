4f47a1654ab5f01e5d4c7c4369152219c9a16928
Bharath Rao <bharath.rao@gmail.com>
2016-12-29 19:49:20 -0800

    add documentation

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # ephemeral-cache
 Cache that expires entries with timeout
+
+# Usage
+
+### Instantiate with default 30s timeout
+```javascript
+var cache = require('ephemeral-cache')
+```
+
+### Change timeout to 30 minutes
+```javascript
+cache.timeout = 30 * 60 * 1000
+```
+
+### Main Operations
+
+```javascript
+
+var value0 = cache.get('key') // returns undefined for non-existent keys
+
+cache.put('key', 'value')
+
+var value1 = cache.get('key') // returns 'value'
+
+var value2 = cache.del('key') // returns 'value' if deleted key exists
+
+var value3 = cache.get('key') // returns undefined for deleted keys
+
+var size = cache.size()
+```
+
+### Reset to clean state; delete everything
+
+```javascript
+cache.reset()
+```
+### Notes
+
+Timeout does not restart on get operation. Explicitly put again if needed.
+
+```javascript
+
+cache.put('key', 'value')
+
+var value = cache.get('key')
+
+cache.put('key', value) // restart timer
+```
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function () {
-  var cache        = {}
-  var map          = {}
-  var size         = 0
-  var cacheTimeout = 30 * 1000
+  var cache     = {}
+  var map       = {}
+  var size      = 0
+  cache.timeout = 30 * 1000
 
-  cache.put = function (uuid, data) {
-    map[uuid] = { data: data, time: Date.now() }
+  cache.put = function (key, data) {
+    map[key] = { data: data, time: Date.now() }
     size++
   }
 
@@ -20,15 +20,6 @@ module.exports = function () {
     return result
   }
 
-  cache.cleanUp = function () {
-    var keys    = Object.keys(map)
-    var current = Date.now()
-    for (var i = 0; i < keys.length; i++) {
-      var time = map[keys[i]].time;
-      if (current - time > cacheTimeout) cache.del(keys[i])
-    }
-  }
-
   cache.size = function() {
     return size
   }
@@ -38,6 +29,17 @@ module.exports = function () {
     size = 0
   }
 
-  setInterval(cache.cleanUp.bind(cache), cacheTimeout)
+  var cleanUp = function () {
+    var keys    = Object.keys(map)
+    var current = Date.now()
+
+    for (var i = 0; i < keys.length; i++) {
+      var time = map[keys[i]].time;
+      if (current - time > cache.timeout) cache.del(keys[i])
+    }
+  }
+
+  setInterval(cleanUp, cache.timeout)
+
   return cache
 }
